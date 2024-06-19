dfc963bb200a6323803b153e163ce1d3b6b92d50
chantroi <duongchantroi@alwaysdata.net>
2024-06-19 11:05:13 +0000

    update 1693 files and delete 481 files

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/tonglanhchua/storage/.venv"
+set -gx VIRTUAL_ENV "/home/tonglanhchua/web-store/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- app.py
+++ app.py
@@ -1,48 +1,38 @@
 from io import BytesIO
-from quart import Quart, request, send_file, render_template, jsonify
+from flask import Flask, request, send_file, render_template, jsonify
 from detafs import DetaFs
 
-app = Quart(__name__)
-fs = DetaFs("c0a1xmuzlhz_9ukoxqAYL8nr8w9hPai2kPbyf7qnAaGC")
+app = Flask(__name__)
+fs = DetaFs("c0Jtyij8pU2_5T5DgCv2L7G6SvpQXR8pU8ujNJbqbW7v")
 
 
 @app.route("/")
-async def index():
+def index():
     files = fs.ls()
-    return await render_template("index.html", items=files)
+    return render_template("index.html", items=files)
 
 
-@app.route("/p/<file>")
-async def stream_file(file: str):
+@app.route("/p/")
+def stream_file(file: str):
     fileobj = BytesIO(fs.open(file).read())
-    return await send_file(fileobj, attachment_filename=file)
+    return send_file(fileobj, attachment_filename=file)
 
 
 @app.route("/file/delete")
-async def delete_route():
+def delete_route():
     target_file = request.args.get("file")
     fs.remove(target_file)
-    return await jsonify(
-        status="success", message=f"file {target_file} deleted successful"
-    )
+    return jsonify(status="success", message=f"file {target_file} deleted successful")
 
 
 @app.route("/file/upload", methods=["POST"])
-async def upload_file():
+def upload_file():
     file = request.files["file"]
     fs.put(file.filename, file.read())
-    return await jsonify(
-        status="success", message=f"file {file.filename} upload completed"
-    )
+    return jsonify(status="success", message=f"file {file.filename} upload completed")
 
 
 @app.route("/file/list")
-async def list_files():
+def list_files():
     files = fs.ls()
-    return await jsonify(files=files)
-
-
-if __name__ == "__main__":
-    from uvicorn import run
-
-    run(app, host="0.0.0.0", port=8080)
+    return jsonify(files=files)
