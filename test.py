450fca1bca3cdd635c142fc7677ddb40317ec5b8
chantroi <duongchantroi@alwaysdata.net>
2024-06-23 11:57:26 +0000

    update 2 files and delete 9 files

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-from detafs import DetaFs
+from pydantic import BaseModel
 
-fs = DetaFs("c0a1xmuzlhz_9ukoxqAYL8nr8w9hPai2kPbyf7qnAaGC")
 
-f = fs.open("Áo Cưới.mp4")
-with open("video2.mp4", "wb") as w:
-    w.write(f.read())
+class Object(BaseModel):
+    name: str
+    age: int
+    gender: str
+
+
+print(Object(name="Natasha", age="28", gender="female").json())
