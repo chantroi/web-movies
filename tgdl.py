f1c2cc24bb28c19f508f65f215224a105fefb321
HF <bosuutao@alwaysdata.net>
2024-06-26 02:11:44 +0000

    update PythonImportHelper-v2-Completion.json and tgdl.py

--- tgdl.py
+++ tgdl.py
@@ -10,17 +10,18 @@ app = Client(
 )
 fs = DetaFs(os.environ["DETA_KEY"])
 
-for i in range(0, 908):
-    try:
-        m = app.get_messages("contentdownload", i)
-        if not m:
-            raise
-        media = app.download_media(m, in_memory=True)
-        filename = media.name
-        data = media.getvalue()
-        fs.put(filename, data)
-        print(filename, " saved")
-        app.send_message(5665225938, filename + " Saved")
-    except Exception as e:
-        print(e)
-        continue
+with app:
+    for i in range(0, 908):
+        try:
+            m = app.get_messages("contentdownload", i)
+            if not m:
+                raise
+            media = app.download_media(m, in_memory=True)
+            filename = media.name
+            data = media.getvalue()
+            fs.put(filename, data)
+            print(filename, " saved")
+            app.send_message(5665225938, filename + " Saved")
+        except Exception as e:
+            print(e)
+            continue
