89c753156e9bb18aa5c42d83c1dbd5bfc72c1391
New Horizon <bosuutap@alwaysdata.net>
2024-06-27 10:09:23 +0000

    update PythonImportHelper-v2-Completion.json, app.py and src/templates/index.html

--- src/app.py
+++ src/app.py
@@ -10,14 +10,9 @@ api_url = f"https://drive.deta.sh/v1/{project_id}/files"
 @app.route("/")
 def index():
     files = fs.list()["names"]
-    return render_template("index.html", files=files)
-
-
-@app.route("/p")
-def stream_file():
-    file = request.args.get("file")
-    res_url = f"{api_url}/files/download?name={file}"
-    return render_template("redirect.html", res_url=res_url, deta_key=deta_key)
+    return render_template(
+        "index.html", files=files, api_url=api_url, deta_key=deta_key
+    )
 
 
 @app.route("/file/delete")
