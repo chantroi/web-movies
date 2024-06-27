b50564d66a70fd953ee508b0edf7aa648f329b5b
New Horizon <bosuutap@alwaysdata.net>
2024-06-27 04:29:54 +0000

    update PythonImportHelper-v2-Completion.json and task.py

--- task.py
+++ task.py
@@ -12,15 +12,8 @@ s3 = boto3.Session(aws_access_key_id=key, aws_secret_access_key=secret).client(
     "s3", endpoint_url=endpoint
 )
 
-for file in s3.list_objects(Bucket="files")["Contents"]:
-    if file["Key"].endswith(".mp4"):
-        path = f'videos/{file["Key"]}'
-    elif file["Key"].endswith(".jpg"):
-        path = f'images/{file["Key"]}'
-    elif file["Key"].endswith(".mp3"):
-        path = f'music/{file["Key"]}'
-    else:
-        path = f'texts/{file["Key"]}'
+for file in s3.list_objects(Bucket="storage")["Contents"]:
+    path = file["Key"]
     data = s3.get_object(Bucket="files", Key=file["Key"])["Body"].read()
     fs.put(path, data)
     print(f"Uploaded {file['Key']} to {path}")
