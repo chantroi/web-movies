5ffdf9be6517b57fea818d48236f93e91e54c1f1
New Horizon <bosuutap@alwaysdata.net>
2024-06-27 08:06:06 +0000

    update PythonImportHelper-v2-Completion.json and task.py

--- task.py
+++ task.py
@@ -9,9 +9,9 @@ s3 = s3fs.S3FileSystem(
 )
 print(s3.ls("storage/video"))
 for file in s3.ls("storage/video"):
-    name = "video/" + file
-    with s3.open("storage/" + name, "rb") as f:
+    with s3.open(name, "rb") as f:
         data = f.read()
+    name = file.split("/", 1)[1]
     fs.put(name, data, content_type="video/mp4")
     print(f"Uploaded {name} to {name}")
 
