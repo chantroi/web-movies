97255e06d66cb2a222d30bca2fdfacc118faa053
New Horizon <bosuutap@alwaysdata.net>
2024-06-27 08:35:10 +0000

    update PythonImportHelper-v2-Completion.json and task.py

--- task.py
+++ task.py
@@ -7,8 +7,10 @@ fs = deta.Deta(deta_key).Drive("files")
 s3 = s3fs.S3FileSystem(
     key=s3_key, secret=s3_secret, client_kwargs={"endpoint_url": s3_endpoint}
 )
-print(s3.ls("storage/video"))
+files = fs.list()["names"]
 for file in s3.ls("storage/video"):
+    if file in files:
+        continue
     with s3.open(file, "rb") as f:
         data = f.read()
     name = file.split("/", 1)[1]
