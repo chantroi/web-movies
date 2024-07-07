39c8d112b14866fc5ba6536f9405dc64e841f775
AzureVM <tonglanhchua@gmail.com>
2024-07-07 07:25:43 +0000

    update 3 files and delete 1 file

---
+++ deta/src/app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, render_template, jsonify
+from deta import Deta
+
+DETA_PROJECT_KEY = os.environ["DETA_PROJECT_KEY"]
+DETA_KEY = os.getenv("DETA_KEY", DETA_PROJECT_KEY)
+
+app = Flask(__name__)
+deta = Deta(DETA_KEY)
+
+
+@app.route("/")
+@app.route("/<drive>")
+def index(drive="files"):
+    fs = deta.Drive(drive)
+    files = fs.list()["names"]
+    return render_template("index.html", files=files, drive=drive)
+
+
+@app.route("/<drive>/<folder>/<file>", methods=["GET"])
+def play_file(drive, folder, file):
+    target_file = folder + "/" + file
+    return render_template(
+        "player.html", drive=drive, filename=target_file, deta_key=DETA_KEY
+    )
+
+
+@app.route("/<drive>/<folder>/<file>", methods=["DELETE"])
+def delete_route(drive, folder, file):
+    target_file = folder + "/" + file
+    fs = deta.Drive(drive)
+    fs.delete(target_file)
+    return jsonify(status="success", message=f"file {target_file} deleted successful")
+
+
+@app.route("/<drive>/<folder>", methods=["GET"])
+def list_files(drive, folder):
+    fs = deta.Drive(drive)
+    files = fs.list()["names"]
+    files = [file for file in files if file.startswith(folder)]
+    return jsonify(files=files)
--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, redirect, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 from deta import Deta
 from environment import deta_key
 
 app = Flask(__name__)
-fs = Deta(deta_key).Drive("files")
+deta = Deta(deta_key)
 
 
 @app.route("/")
-def index():
+@app.route("/<drive>")
+def index(drive="files"):
+    fs = deta.Drive(drive)
     files = fs.list()["names"]
     return render_template(
         "index.html",
@@ -15,36 +17,42 @@ def index():
     )
 
 
-@app.route("/file/<folder>/<file>", methods=["GET"])
-def play_file(folder, file):
+@app.route("/<drive>/<folder>/<file>", methods=["GET"])
+def play_file(drive, folder, file):
     target_file = folder + "/" + file
-    return render_template("player.html", filename=target_file, deta_key=deta_key)
+    return render_template(
+        "player.html", drive=drive, filename=target_file, deta_key=deta_key
+    )
 
 
-@app.route("/file/delete")
-def delete_route():
-    target_file = request.args.get("file")
+@app.route("/<drive>/<folder>/<filename>", methods=["DELETE"])
+def delete_route(drive, folder, filename):
+    fs = deta.Drive(drive)
+    target_file = folder + "/" + filename
     fs.delete(target_file)
     return jsonify(status="success", message=f"file {target_file} deleted successful")
 
 
-@app.route("/file/upload", methods=["POST"])
-def upload_file():
+@app.route("/<drive>/<folder>", methods=["POST"])
+def upload_file(drive, folder):
+    fs = deta.Drive(drive)
     if request.files:
         file = request.files["file"]
-        fs.put(file.filename, file.read())
+        fs.put(folder + "/" + file.filename, file.read())
         return jsonify(
             status="success", message=f"file {file.filename} upload completed"
         )
     else:
         data = request.json
-        fs.put(data["name"], data["content"])
+        fs.put(folder + "/" + data["name"], data["content"])
         return jsonify(
             status="success", message=f"file {data['name']} upload completed"
         )
 
 
-@app.route("/file/list")
-def list_files():
+@app.route("/<drive>/<folder>")
+def list_files(drive, folder):
+    fs = deta.Drive(drive)
     files = fs.list()["names"]
+    files = [file for file in files if file.startswith(folder)]
     return jsonify(files=files)
