f7f141d77b1a956bf84dc4950d546f413d7027a6
AzureVM <tonglanhchua@gmail.com>
2024-07-09 05:31:31 +0000

    update PythonImportHelper-v2-Completion.json and task.py

--- task.py
+++ task.py
@@ -2,7 +2,12 @@ import pyrogram
 import deta
 
 deta = deta.Deta("c0kEEGmHJte_YjH9AKDzdmP4tm6Zyge3Fme9KyMRNwXB")
-bot = pyrogram.Client("uploader", 21021245, "7b32ea92719781c5e22ede319c5dbde5")
+bot = pyrogram.Client(
+    "uploader",
+    21021245,
+    "7b32ea92719781c5e22ede319c5dbde5",
+    bot_token="5846865945:AAFdGzRy-1-KZXZOm1je_oR-LQTsCOCHfqI",
+)
 drive = deta.Drive("files")
 bot.start()
 for i in range(1, 1672):
