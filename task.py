5a09df85d3f78e8e5a1cb70e477e030b59e74417
AzureVM <tonglanhchua@gmail.com>
2024-07-09 05:28:39 +0000

    chore: update PythonImportHelper-v2-Completion.json

---
+++ task.py
@@ -0,0 +1,15 @@
+import pyrogram
+import deta
+
+deta = deta.Deta("c0kEEGmHJte_YjH9AKDzdmP4tm6Zyge3Fme9KyMRNwXB")
+bot = pyrogram.Client("uploader", 21021245, "7b32ea92719781c5e22ede319c5dbde5")
+drive = deta.Drive("files")
+bot.start()
+for i in range(1, 1672):
+    m = bot.get_messages("contentdownload", i)
+    if m.video:
+        print(i)
+        video = bot.download_media(m, in_memory=True)
+        name = video.name
+        drive.put(name, video.getvalue())
+        print(name)
