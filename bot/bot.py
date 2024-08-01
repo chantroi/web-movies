ff5a949024a305ae8ec8245589d76d98a5fe9ed9
New Horizon <tonglanhchua@gmail.com>
2024-08-01 12:16:20 +0000

    update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.enums import ChatAction
-from .util import Storage
+from bot.util import Storage
 from environment import bot_token
 
 bot = Client(
